ad7f27b0e1d2439849d9400e47639b62c5e17802
Prateek <prateek.kumar.original@gmail.com>
2023-07-10 15:51:37 +0530

    Add layouts and work list

--- app.js
+++ app.js
@@ -7,22 +7,40 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"))
 
-let items = ['Eat', 'Code', 'Sleep'];
+let newListItems = ['Eat', 'Code', 'Sleep'];
+let workItems = [];
 
 app.get("/", (req, res) => {
     const today = new Date();
     let options = { weekday: 'long', month: 'long', day: 'numeric' }
     let day = today.toLocaleDateString('en-us', options);
 
-    res.render("list", { day: day, newItems: items });
+    res.render("list", { listTitle: day, newListItems: newListItems });
 });
 
 app.post("/", (req, res) => {
     if ((x = req.body.newItem) !== "") {
-        items.push(x);
+        if (req.body.list === 'Work List') {
+            workItems.push(x)
+            res.redirect("/work");
+        } else {
+            newListItems.push(x);
+        }
     }
     res.redirect("/");
 });
+
+app.get("/work", (req, res) => {
+    res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.post("/work", (req, res) => {
+    res.redirect("/work");
+});
+
+app.get("/about", (req, res)=>{
+    res.render("about", {listTitle: "About Me"});
+})
 
 app.listen(3000, () => {
     console.log("Server running on http://127.0.0.1:3000");
